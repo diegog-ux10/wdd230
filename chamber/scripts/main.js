52d201cfb6153d5b88c6788464739155bf1e3390
diegogranados143@gmail.com <diego@codeando.edu.pe>
2024-05-19 20:45:53 -0400

    fixing chamber site

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -2,7 +2,9 @@ const menu = document.getElementById('main-nav');
 const btMenu = document.getElementById('menu-toggle');
 const hamburgerMenu = document.getElementById('menu-icon') 
 const closeMenuIcon = document.getElementById('close-icon') 
-
+document.getElementById("year").innerHTML = new Date().getFullYear();
+document.getElementById("lastModified").innerHTML = new Date();
+console.log("gi");
 
 const toggleMenu = () => {
     menu.classList.toggle('active');
